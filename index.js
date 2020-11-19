8e41fbade5d1c71cf1cf971657c584b81f5bc4bc
unknown <gs_arruda@outlook.com>
2020-11-19 17:31:24 -0300

    update

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const app = express();
 app.use(express.json());
 app.use(cors(), routes)
 
-const port = 3333 || process.env.PORT;
+const port = process.env.PORT || 3030;
 app.listen(port, () => { console.log(`Server Online on ${port}`) })
